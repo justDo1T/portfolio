5afad3b9050c8eef8e71d347df05385140caf38c
Adrian Siewiera <siewiera@gmail.com>
2017-07-05 15:07:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio
+
+This is the portfolio website of polish programmers, who are creating advanced and complex **web/mobile applications** for startups and enterprises.
+
+## Front-end team:
+- [Adrian Siewiera](https://www.facebook.com/adrian.siewiera)
+- [Tomasz Piątek](https://www.facebook.com/tomasz.piatek.395)
+
+## Back-end team:
+- [Wojtek Samek](https://www.facebook.com/wojoj1993)
+- [Jarek Błażków](https://www.facebook.com/jarekblazkow)
---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+	// Changing background of top navigation bar when page is scrolled
+	$(document).on('scroll', function() {
+		var topNav = $('#top-bar-wrapper');
+    	topNav.toggleClass('scrolled', $(this).scrollTop() > topNav.height());
+    });
+    if ($(window).scrollTop() > 100) {
+        $('#top-bar-wrapper').addClass('scrolled');
+    } else {
+        $('#top-bar-wrapper').removeClass('scrolled');
+    }
+
+	// Transforming hamburger menu-icon to closing X-icon
+    $('#menu-icon').on('click', function() {
+    	$(this).toggleClass('change');
+    	$('#main-nav').toggleClass('showHide');
+    });
+
+    // Flip effect
+    $('.flip-container').on('click', function() {
+    	$(this).toggleClass('clicked');
+    });
+
+    // Slide show/hide team-member details
+    $('.about-button').on('click', function() {
+    	$(this).parent().children('.about-member').slideToggle();
+    });
+
+    // Show/hide google maps in contact area
+    $('#map-button').on('click', function() {
+    	$('#contact-container').toggleClass('mapToggle');
+    	$('#google-map-container').toggleClass('active');
+    	$(this).text(function(i, text) {
+    		return text === "Show map" ? "Hide map" : "Show map";
+    	}); 
+    });
+
+    // Google map active only after click
+    $('#google-map').addClass('mapScrollOff');
+	$('#google-map-container').on('click', function () {		// when clicked, set the mouse events free
+        $('#google-map').removeClass('mapScrollOff');
+    });
+    $('#google-map').mouseleave(function () {		// when mouse leaves the map, disable scroll again
+    	$(this).addClass('mapScrollOff');
+   	});
+
+    // Smooth scroll navigation links (https://github.com/flesler/jquery.scrollTo)
+    $.extend($.scrollTo.defaults, {
+    	// change default settings here
+    	// adjusting to fixed navigation bar, which is always on top (width: 82px)
+    	offset: {top: -82}
+	});
+	// after comma put time length in ms
+	$('#logo').on('click', function() {
+   		$(window).scrollTo($('body'), 1000);
+   	});
+   	$('#link-portfolio, #start-button').on('click', function() {
+   		$(window).scrollTo($('#portfolio'), 1000);
+   	});
+   	$('#link-offer').on('click', function() {
+   		$(window).scrollTo($('#offer'), 1000);
+   	});
+   	$('#link-technology').on('click', function() {
+   		$(window).scrollTo($('#technology'), 1000);
+   	});
+    $('#link-team').on('click', function() {
+   		$(window).scrollTo($('#team'), 1000);
+   	});
+   	$('#link-contact').on('click', function() {
+   		$(window).scrollTo($('#contact'), 1000);
+   	});
+});
